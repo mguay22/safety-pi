e8251271cf1d3846350585a10093319fe6412c52
Michael Guay <mguay22@Michaels-MacBook-Pro-5.local>
2018-03-28 19:12:08 -0400

    Setup Google Cloud, SDK, and testpython script

---
+++ speechTest.py
@@ -0,0 +1,37 @@
+# Demo speech test, code taken from https://cloud.google.com/speech/docs/reference/libraries#client-libraries-install-python
+# https://googlecloudplatform.github.io/google-cloud-python/latest/speech/index.html
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-request.json"
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'audio.raw')
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
