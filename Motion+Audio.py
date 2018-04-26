6893bd45b5c4cb21cb8909edb4594843d2df7733
mguay22 <guay.me@gmail.com>
2018-04-26 19:47:06 -0400

    Added text to speech and cleaned up files

--- Motion+Audio.py
+++ Motion+Audio.py
@@ -1,8 +1,4 @@
 import time
-# Use Cloudant to create a Cloudant client using account
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-from cloudant.result import Result, ResultByKey
 from gpiozero import MotionSensor
 
 #Imports for pyaudio
@@ -44,45 +40,6 @@ def recordAudio():
     waveFile.setframerate(RATE)
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
-
-
-def incrementCentralPopulation():
-    username='3e6d30ee-d974-4ed8-a6b7-1504c9b01df6-bluemix'
-    password = "72d189c906f2630a7793ac0918894d55ed07904548da1ca6ae4f274cacf7aa00"
-    URL="https://3e6d30ee-d974-4ed8-a6b7-1504c9b01df6-bluemix:72d189c906f2630a7793ac0918894d55ed07904548da1ca6ae4f274cacf7aa00@3e6d30ee-d974-4ed8-a6b7-1504c9b01df6-bluemix.cloudant.com"
-
-    client = Cloudant(username,password,url=URL)
-
-    client.connect()
-    dbname='codefest-info'
-    db=client[dbname]
-    '''
-    results=Result(db.all_docs, include_docs=True)
-    print ("Retrieved minimal document:\n{0}\n".format(results[0]))
-    '''
-
-    '''
-    # First retrieve the document
-    grundle = db['089061c18a884da9a718b16b76807712']
-    grundle['currentTotal'] += 1
-    # You must save the document in order to update it on the database
-    grundle.save()
-    '''
-
-    # First retrieve the document
-    central = db['59b03acc7eedb2b21639ff88f15a671e']
-    central['currentTotal'] += 1
-    # You must save the document in order to update it on the database
-    central.save()
-
-
-    '''
-    session = client.session()
-    database= client["codefest-info"]
-    '''
-
-    # Disconnect from the server
-    client.disconnect()
 
 pir = MotionSensor(4)
 
--- speech.py
+++ speech.py
@@ -1,13 +1,52 @@
+import time
+from gpiozero import MotionSensor
+import os
+import json
+import mysql.connector
+import pyaudio
+import wave
+import io
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+
+def recordAudio():
+    FORMAT = pyaudio.paInt32
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = "go.wav"
+     
+    audio = pyaudio.PyAudio()
+     
+    # start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    print("recording...")
+    frames = []
+     
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK, exception_on_overflow = False)
+        frames.append(data)
+    print("finished recording")
+     
+     
+    # stop Recordings
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+     
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
 def transcribeFile(fileName):
-
-    import io
-    import os
-
-    # Imports the Google Cloud client library
-    from google.cloud import speech
-    from google.cloud.speech import enums
-    from google.cloud.speech import types
-
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="service-request.json"
     os.system("avconv -i " + fileName + ".wav -ac 1 " + fileName + ".flac")
 
@@ -31,14 +70,16 @@ def transcribeFile(fileName):
 
     # Detects speech in the audio file
     response = client.recognize(config, audio)
+
+    if response is None:
+        output = ''
+
     for result in response.results:
         output = result.alternatives[0].transcript
 
     return output
 
 def queryDatabase(output):
-    import mysql.connector
-
     cnx = mysql.connector.connect(user='mguay', password='ramsamsam123', host='localhost', database='safetypi')
     cursor = cnx.cursor()
     query = ("SELECT * FROM USERS;")
@@ -46,19 +87,13 @@ def queryDatabase(output):
 
     for id in cursor:
         for value in id:
-            if value == output:
-                cursor.close()
-                cnx.close()
+            if str(value) == str(output):
                 return True
-                
-    cursor.close()
-    cnx.close()
+
     return False
 
 
 def addUser(id):
-    import mysql.connector
-
     cnx = mysql.connector.connect(user='mguay', password='ramsamsam123', host='localhost', database='safetypi')
     cursor = cnx.cursor()
 
@@ -72,17 +107,60 @@ def addUser(id):
     cursor.close()
     cnx.close()
 
+def textToSpeech(text):
+    os.system("curl -H \"Authorization: Bearer \"$(gcloud auth application-default print-access-token) -H \"Content-Type: application/json; charset=utf-8\" --data \"{\'input\':{\'text\': \'" + text + "\'},\'voice\':{\'languageCode\':\'en-gb\',\'name\':\'en-GB-Standard-A\',\'ssmlGender\':\'FEMALE\'},\'audioConfig\':{\'audioEncoding\':\'MP3\'}}\" \"https://texttospeech.googleapis.com/v1beta1/text:synthesize\" > synthesize-output.json")
+
+    with open('synthesize-output.json') as data_file:    
+        data = json.load(data_file)
+
+    content = data['audioContent']
+    file = open("synthesize-output-base64.txt", "w+")
+    file.write(content)
+    file.close()
+
+    os.system("base64 synthesize-output-base64.txt --decode > synthesized-audio.mp3")
+
+    os.remove("synthesize-output.json")
+    os.remove("synthesize-output-base64.txt")
+
+    # Then play audio
+    
+
+
 
 def main():
-    output = transcribeFile("go")
-    print(output)
-    result = queryDatabase(output)
+    pir = MotionSensor(4)
+    while True:
+        count = 1
+        authenticated = False
+        if pir.motion_detected:
+            while (authenticated == False):
+                if (count == 4):
+                    # Send text to admin
+                    print("Intruder alert")
+                    return
 
-    if result:
-        # User has been authenticated
-        print("Welcome")
-    else:
-        print("Please try again")
-        # Try again two more times
+                print("Motion detected")
+                recordAudio()
+                output = transcribeFile("go")
+                os.remove("go.wav")
+                os.remove("go.flac")
+                result = queryDatabase(output)
+                if result:
+                    # User has been authenticated
+                    # Play authentication sound
+                    print("Welcome")
+                    authenticated = True
+                    return
+                else:
+                    # Play incorrect code sound, let the user try again, max 3 times
+                    print("Please try again")
+                    count += 1
+            
+        else:
+            print("No motion detected")
+
+    # textToSpeech("hello")
+
 
 main()
